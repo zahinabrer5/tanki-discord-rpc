5ceff487e3f9943211a2b360a88757399c77d4d6
Zahin <zahinabrer5@gmail.com>
2025-01-05 10:54:47 -0500

    add option to use simple mode (start RPC when program starts, end with program ends (or Discord is closed))

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const getRank = (rawRank) => {
 // return a string that has thousands separators for integer n
 // https://stackoverflow.com/a/2901298/21405641
 const thousandsSep = n => {
-    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    return n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
 };
 
 let username = 'milk_Dud';
@@ -27,11 +27,12 @@ let rank = 1;
 let hasPremium = false;
 
 // pass username as command line argument
-if (process.argv.length === 2) {
-    console.error('Expected at least one argument!');
+if (process.argv.length < 4) {
+    console.error('Expected at least two arguments!');
     process.exit(1);
 }
 username = process.argv[2]
+const simpleMode = process.argv[3] === 'true'
 
 // https://nodejs.org/en/blog/announcements/v18-release-announce/#fetch-experimental
 const res = await fetch(`https://ratings.tankionline.com/api/eu/profile/?user=${username}&lang=en`);
@@ -51,30 +52,14 @@ let rankFile = `icons${hasPremium ? 'premium' : 'normal'}_${('00' + Math.min(31,
 const rpc = new Client({ transport: 'ipc' });
 let presenceActive = false;
 
-// CORS middleware to allow requests from extension
-app.use(cors());
-
-// Middleware to authenticate requests
-app.use((req, res, next) => {
-    const apiKey = req.headers['x-api-key'];
-    if (apiKey !== API_KEY)
-        return res.status(403).send('Forbidden');
-    next();
-});
-
 rpc.on('ready', () => {
     console.log(`Tanki Rich Presence (for user ${username}) is now active!`);
 });
 
-app.get('/startRPC', (req, res) => {
-    if (presenceActive) {
-        res.json({ message: "RPC Already Active" });
-        return;
-    }
-
+const startRPC = (usingSimpleMode, res) => {
     // Log in to Discord only once and wait for the 'ready' event
     rpc.login({ clientId }).then(() => {
-        console.log("RPC Login successful.");
+        console.log('RPC Login successful.');
         presenceActive = true;
 
         // Now that the client is ready, we can set the activity
@@ -100,29 +85,56 @@ app.get('/startRPC', (req, res) => {
                 }
             ]
         });
-        res.json({ message: "RPC Started" });
+        if (!usingSimpleMode)
+            res.json({ message: 'RPC Started' });
     }).catch(err => {
-        console.error("Error logging into Discord RPC:", err);
-        res.status(500).json({ message: "Error logging into Discord RPC" });
+        console.error('Error logging into Discord RPC:', err);
+        if (!usingSimpleMode)
+            res.status(500).json({ message: 'Error logging into Discord RPC' });
     });
-});
+};
 
-app.get('/stopRPC', (req, res) => {
-    if (!presenceActive) {
-        res.json({ message: "RPC Not Active" });
-        return;
-    }
+if (simpleMode) {
+    startRPC(simpleMode);
+}
+else {
+    // CORS middleware to allow requests from extension
+    app.use(cors());
 
-    rpc.clearActivity().then(() => {
-        console.log("RPC Stopped");
-        presenceActive = false;
-        res.json({ message: "RPC Stopped" });
-    }).catch(err => {
-        console.error("Error stopping RPC:", err);
-        res.status(500).json({ message: "Error stopping RPC" });
+    // Middleware to authenticate requests with API key
+    app.use((req, res, next) => {
+        const apiKey = req.headers['x-api-key'];
+        if (apiKey !== API_KEY)
+            return res.status(403).send('Forbidden');
+        next();
     });
-});
 
-app.listen(3000, '127.0.0.1', () => {
-    console.log('Server is running on http://localhost:3000');
-});
+    app.get('/startRPC', (req, res) => {
+        if (presenceActive) {
+            res.json({ message: 'RPC Already Active' });
+            return;
+        }
+
+        startRPC(simpleMode, res);
+    });
+
+    app.get('/stopRPC', (req, res) => {
+        if (!presenceActive) {
+            res.json({ message: 'RPC Not Active' });
+            return;
+        }
+
+        rpc.clearActivity().then(() => {
+            console.log('RPC Stopped');
+            presenceActive = false;
+            res.json({ message: 'RPC Stopped' });
+        }).catch(err => {
+            console.error('Error stopping RPC:', err);
+            res.status(500).json({ message: 'Error stopping RPC' });
+        });
+    });
+
+    app.listen(3000, '127.0.0.1', () => {
+        console.log('Server is running on http://localhost:3000');
+    });
+}
